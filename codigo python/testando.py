a2a78fa07d5a7594b02ab157d9024984bdb274b4
Anderson <andersonsantosdacosta3084@gmail.com>
2025-12-17 15:13:47 -0500

    média

--- codigo python/testando.py
+++ codigo python/testando.py
@@ -1,4 +1,8 @@
-print('testando uns comando')
-x=int(input('digite um número: '))
-y=int(input('digite mais um número: '))
-print(x+y)
+y=0
+print('Vamos calcular a média!')
+for x in range(3):
+    x=int(input('Digite um número: '))
+    y+=x
+
+z=y/3
+print(f'A soma dos valores é {y} com média igual {z:.2f}')
