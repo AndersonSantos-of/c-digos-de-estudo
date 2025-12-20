f433e291e66626419de3100ffcb41b52f76096ce
Anderson <andersonsantosdacosta3084@gmail.com>
2025-12-20 16:35:05 -0500

    ajustes

--- códigos python/função teste.py
+++ códigos python/função teste.py
@@ -1,6 +1,6 @@
 def soma(a,b):
     x=a+b
-    print(f'A soma de [{a} + {b}] é  igual a [{x}].',)
+    print(f'A soma de [{a}] + [{b}] é  igual a [{x}].',)
 
 x=int(input('Digite um número inteiro: '))
 y=int(input('Digite outro número inteiro: '))
