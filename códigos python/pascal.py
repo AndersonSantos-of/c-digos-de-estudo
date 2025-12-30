e179221a25adc00ff44291f87da6d918e74012bf
Anderson Santos <127549082+AndersonSantos-of@users.noreply.github.com>
2025-12-30 16:59:41 -0500

    triangulo pascal

--- códigos python/pascal.py
+++ códigos python/pascal.py
@@ -1,3 +1,14 @@
-x= 3 + 5
+def pascal(n, k):
+    if k==0 or k==n:
+        return 1
+    return pascal(n-1, k-1)+pascal(n-1,k)
 
-print(x)
+def pascal_piramide(linha):
+    for n in range(linha):
+        print(' ' * (linha-n), end="")
+
+        for k in range(n+1):
+            print(pascal(n,k), end=' ')
+        print()
+
+pascal_piramide(5)
